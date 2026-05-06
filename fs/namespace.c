1ba2173f983144217133105bba9f21adf4b46c25
Admin <nanvanu@gmail.com>
2026-05-06 12:37:00 +0700

    fix kern_mount

--- fs/namespace.c
+++ fs/namespace.c
@@ -1261,15 +1261,15 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 		return ERR_PTR(-ENODEV);
 
 #if defined(CONFIG_KSU_SUSFS_SUS_MOUNT) && !defined(CONFIG_KSU_SUSFS_MODIFY)
-	// For newly created mounts, the only caller process we care is KSU
-	//if (!susfs_is_sdcard_android_data_decrypted && unlikely(susfs_is_current_ksu_domain())) {
-	//	mnt = alloc_vfsmnt(name, true, 0);
-	//	goto bypass_orig_flow;
-	//}
-	mnt = alloc_vfsmnt(name, false, 0);
-bypass_orig_flow:
-#else
+	 For newly created mounts, the only caller process we care is KSU
+	if (!susfs_is_sdcard_android_data_decrypted && unlikely(susfs_is_current_ksu_domain())) {
+		mnt = susfs_alloc_non_unshare_ksu_vfsmnt(name ?: "none");
+		goto bypass_orig_flow;
+	}
+#endif
 	mnt = alloc_vfsmnt(name);
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+bypass_orig_flow:
 #endif
 	if (!mnt)
 		return ERR_PTR(-ENOMEM);
