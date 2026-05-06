1dcdc1573a414bff0b5be674db6fa018dbe240e6
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-06 13:33:05 +0700

    Update namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -1159,11 +1159,11 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 		return ERR_PTR(-ENODEV);
 
 #if defined(CONFIG_KSU_SUSFS_SUS_MOUNT) && !defined(CONFIG_KSU_SUSFS_MODIFY)
-	// For newly created mounts, the only caller process we care is KSU
-	//if (!susfs_is_sdcard_android_data_decrypted && unlikely(susfs_is_current_ksu_domain())) {
-	//	mnt = alloc_vfsmnt(name, true, 0);
-	//	goto bypass_orig_flow;
-	//}
+	 For newly created mounts, the only caller process we care is KSU
+	if (!susfs_is_sdcard_android_data_decrypted && unlikely(susfs_is_current_ksu_domain())) {
+		mnt = alloc_vfsmnt(name, true, 0);
+		goto bypass_orig_flow;
+	}
 	mnt = alloc_vfsmnt(name, false, 0);
 bypass_orig_flow:
 #else
@@ -1224,6 +1224,7 @@ static struct mount *clone_mnt(struct mount *old, struct dentry *root,
 	struct mount *mnt;
 	int err;
 #if defined(CONFIG_KSU_SUSFS_SUS_MOUNT) && !defined(CONFIG_KSU_SUSFS_MODIFY)
+	goto bypass_orig_flow;
 	// We won't check it anymore if boot-completed stage is triggered.
 	if (susfs_is_sdcard_android_data_decrypted) {
 		goto orig_flow;
