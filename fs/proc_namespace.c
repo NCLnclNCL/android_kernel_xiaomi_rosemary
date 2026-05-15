e5dbab32ea885964ce10aa4ac330c204dd32ed4f
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-15 09:44:29 +0700

    test  && !susfs_is_current_ksu_domain()

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -152,7 +152,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 	//uid_t current_uid_val = current_uid().val;
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
+	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID)))
 	//	pr_info("%s: mountinfo by: %d with process: %s\n", __func__, current_uid_val, current->comm);
 		return 0;
 #endif
